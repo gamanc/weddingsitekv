4632d9abce8f40177a68e32b2e19077a781dc393
Gama <gammanolasco7@gmail.com>
2023-07-26 00:41:27 -0600

    feat: add landing page

--- src/slices/LandingSection/index.tsx
+++ src/slices/LandingSection/index.tsx
@@ -1,28 +1,64 @@
 import { Content } from "@prismicio/client";
-import { PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
-/**
- * Props for `LandingSection`.
- */
+import { Cinzel_Decorative, Vujahday_Script, Cinzel } from "next/font/google";
+import styles from "./styles.module.css";
+import clsx from "clsx";
+import Image from "next/image";
+
+const titleFont = Cinzel_Decorative({ weight: "700", subsets: ["latin"] });
+const subtitleFont = Cinzel({ subsets: ["latin"], weight: "600" });
+const manuscriptFont = Vujahday_Script({
+  weight: "400",
+  subsets: ["latin-ext"],
+});
+
 export type LandingSectionProps =
   SliceComponentProps<Content.LandingSectionSlice>;
 
-/**
- * Component for "LandingSection" Slices.
- */
 const LandingSection = ({ slice }: LandingSectionProps): JSX.Element => {
-  const { title, subtitle, backgroundImage } = slice.primary;
+  const { subtitle } = slice.primary;
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={styles.container}
     >
-      <PrismicRichText field={title} />
-      <PrismicRichText field={subtitle} />
-      <PrismicNextImage
-        style={{ width: "100vw", height: "300px", objectFit: "cover" }}
-        field={backgroundImage}
+      <div className={clsx(styles.mainTitle, titleFont.className)}>
+        <p className={styles.names}>Kevin</p>
+        <p className={clsx(styles.connector, manuscriptFont.className)}>&</p>
+        <p className={styles.names}>Vanesa</p>
+
+        <div className={clsx(styles.line, styles.leftLine)} />
+        <div className={clsx(styles.line, styles.rightLine)} />
+      </div>
+      <PrismicRichText
+        field={subtitle}
+        components={{
+          heading2: ({ children }) => (
+            <h2 className={clsx(styles.subtitle, subtitleFont.className)}>
+              {children}
+            </h2>
+          ),
+        }}
+      />
+      <Image
+        width={772}
+        height={760}
+        draggable={false}
+        priority
+        className={styles.topOrnament}
+        src="/img/floral-ornament-hd.png"
+        alt="ornament"
+      />
+      <Image
+        width={772}
+        height={760}
+        draggable={false}
+        priority
+        className={styles.bottomOrnament}
+        src="/img/floral-ornament-hd.png"
+        alt="ornament"
       />
     </section>
   );
