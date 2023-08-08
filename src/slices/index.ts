55aeeaca7d681d5259d13b15d18db1c8407e501b
Gama <gammanolasco7@gmail.com>
2023-08-08 00:13:29 -0600

    chore: add missing files for new welcome slice

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -4,4 +4,5 @@ import dynamic from "next/dynamic";
 
 export const components = {
   landing_section: dynamic(() => import("./LandingSection")),
+  welcome_section: dynamic(() => import("./WelcomeSection")),
 };
