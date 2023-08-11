7449f5ddae463b7f30bf6fdc044498b8189ff075
Gama <gammanolasco7@gmail.com>
2023-08-11 01:18:27 -0600

    feat: add locations slice

--- src/components/FloralOrnament/index.tsx
+++ src/components/FloralOrnament/index.tsx
@@ -5,16 +5,29 @@ import clsx from "clsx";
 interface Props {
   className: string;
   rotated?: boolean;
+  flipped?: boolean;
 }
 
-const FloralOrnament = ({ className, rotated = false }: Props) => {
+const FloralOrnament = ({
+  className,
+  rotated = false,
+  flipped = false,
+}: Props) => {
   return (
     <Image
       width={772}
       height={760}
       draggable={false}
       priority
-      className={clsx(className, { rotated: rotated }, "block-touch-callout")}
+      className={clsx(
+        className,
+        {
+          rotated: rotated,
+          flipped: flipped,
+          "rotated-flipped": rotated && flipped,
+        },
+        "block-touch-callout"
+      )}
       src="/img/floral-ornament-hd.png"
       alt="ornament"
     />
--- src/slices/LocationsSection/index.tsx
+++ src/slices/LocationsSection/index.tsx
@@ -1,5 +1,11 @@
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { cinzel, DMSans } from "@/app/fonts";
+import clsx from "clsx";
+
+import styles from "./styles.module.scss";
+import { PrismicNextLink } from "@prismicio/next";
+import FloralOrnament from "@/components/FloralOrnament";
 
 /**
  * Props for `LocationsSection`.
@@ -11,13 +17,54 @@ export type LocationsSectionProps =
  * Component for "LocationsSection" Slices.
  */
 const LocationsSection = ({ slice }: LocationsSectionProps): JSX.Element => {
+  const {
+    ceremonyLocationName,
+    ceremonyLocationAddress,
+    ceremonyLocationLink,
+    receptionLocationName,
+    receptionLocationAddress,
+    receptionLocationLink,
+  } = slice.primary;
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={clsx("full-screen", styles.container)}
     >
-      Placeholder component for locations_section (variation: {slice.variation})
-      Slices
+      <div className={styles.innerContainer}>
+        <div className={styles.locationContainer}>
+          <span className={clsx(styles.eyebrowText, cinzel.className)}>
+            Ceremonia
+          </span>
+          <h1 className={styles.locationTitle}>{ceremonyLocationName}</h1>
+          <div className={styles.line} />
+          <PrismicNextLink
+            className={clsx(styles.locationAddressLink, DMSans.className)}
+            target="_blank"
+            field={ceremonyLocationLink}
+          >
+            {ceremonyLocationAddress}
+          </PrismicNextLink>
+        </div>
+
+        <div className={clsx(styles.locationContainer, styles.bottomContainer)}>
+          <span className={clsx(styles.eyebrowText, cinzel.className)}>
+            Recepción
+          </span>
+          <h1 className={styles.locationTitle}>{receptionLocationName}</h1>
+          <div className={styles.line} />
+          <PrismicNextLink
+            className={clsx(styles.locationAddressLink, DMSans.className)}
+            target="_blank"
+            field={receptionLocationLink}
+          >
+            {receptionLocationAddress}
+          </PrismicNextLink>
+        </div>
+      </div>
+
+      <FloralOrnament className={styles.ornament} flipped rotated />
     </section>
   );
 };
