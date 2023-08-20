a8a53fefe346e1700ed8f47e51078700564f2308
Gama <gammanolasco7@gmail.com>
2023-08-20 10:52:05 -0600

    feat: move divider ornament to separate component

---
+++ src/components/DividerOrnament/index.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import clsx from "clsx";
+import styles from "./styles.module.scss";
+
+const DividerOrnament = () => {
+  return (
+    <Image
+      className={clsx("block-touch-callout", styles.dividerOrnament)}
+      src="/img/divider-ornament.png"
+      width={1107}
+      height={162}
+      alt="divider"
+      draggable={false}
+      priority
+    />
+  );
+};
+
+export default DividerOrnament;
