3b6e69e05292bb4f494b526a412adcc40e2dfd9d
Gama <gammanolasco7@gmail.com>
2023-09-03 01:16:22 -0600

    style(DividerOrnament): add blueFilter class to change color

--- src/components/DividerOrnament/index.tsx
+++ src/components/DividerOrnament/index.tsx
@@ -5,7 +5,11 @@ import styles from "./styles.module.scss";
 const DividerOrnament = () => {
   return (
     <Image
-      className={clsx("block-touch-callout", styles.dividerOrnament)}
+      className={clsx(
+        "block-touch-callout",
+        styles.dividerOrnament,
+        styles.blueFilter
+      )}
       src="/img/divider-ornament.png"
       width={1107}
       height={162}
